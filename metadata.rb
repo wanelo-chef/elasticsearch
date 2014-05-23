2bfec7f2a1d6f908fff1c0215624bb00101bc99f
Doc Ritezel & Eric Saxby <doc+sax@wanelo.com>
2014-05-22 17:01:21 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'play@wanelo.com'
 license          'MIT'
 description      'Installs/Configures elasticsearch on Joyent SmartOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
