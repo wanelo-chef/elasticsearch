52c7f5381e9d3391985ab20eef6219f572548100
Doc Ritezel & Konstantin Gredeskoul <doc+kig@wanelo.com>
2014-06-03 16:31:26 -0700

    Fixing failing test on Travis by simplifying

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,27 +1,17 @@
 require 'spec_helper'
 
 describe 'elasticsearch::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
-
   before do
-    # Don't worry about external cookbook dependencies
-    Chef::Cookbook::Metadata.any_instance.stub(:depends)
-
-    # Test each recipe in isolation, regardless of includes
-    @included_recipes = []
-    Chef::RunContext.any_instance.stub(:loaded_recipe?).and_return(false)
-    Chef::Recipe.any_instance.stub(:include_recipe) do |i|
-      Chef::RunContext.any_instance.stub(:loaded_recipe?).with(i).and_return(true)
-      @included_recipes << i
-    end
-    Chef::RunContext.any_instance.stub(:loaded_recipes).and_return(@included_recipes)
+    Chef::Recipe.any_instance.stub(:include_recipe)
   end
 
   it 'includes the configure recipe' do
-    expect(chef_run).to include_recipe('elasticsearch::configure')
+    Chef::Recipe.any_instance.should_receive(:include_recipe).with('elasticsearch::configure')
+    ChefSpec::Runner.new.converge(described_recipe)
   end
 
   it 'includes the install recipe' do
-    expect(chef_run).to include_recipe('elasticsearch::install')
+    Chef::Recipe.any_instance.should_receive(:include_recipe).with('elasticsearch::install')
+    ChefSpec::Runner.new.converge(described_recipe)
   end
 end
