8e558eeee2a4bb78915e2f221a1f66e0657f3065
Doc Ritezel & Tori Scallan <doc+tori@wanelo.com>
2014-06-12 18:41:04 -0700

    Don't check blank? on a string

--- recipes/newrelic.rb
+++ recipes/newrelic.rb
@@ -10,17 +10,17 @@ include_recipe 'elasticsearch::service'
 template '/opt/local/newrelic/newrelic.yml' do
   source 'newrelic.yml.erb'
   variables(:newrelic => node[:elasticsearch][:newrelic])
-  not_if { node[:elasticsearch][:newrelic][:api_key].blank? }
+  not_if { node[:elasticsearch][:newrelic][:api_key].to_s.empty? }
 end
 
 remote_file '/opt/local/newrelic/newrelic.jar' do
   source node[:elasticsearch][:newrelic][:jar_url]
-  not_if { node[:elasticsearch][:newrelic][:api_key].blank? }
+  not_if { node[:elasticsearch][:newrelic][:api_key].to_s.empty? }
 end
 
 template '/opt/local/bin/elasticsearch.in.sh' do
   source 'elasticsearch.in.sh.erb'
   mode 0755
   notifies :restart, 'service[elasticsearch]'
-  not_if { node[:elasticsearch][:newrelic][:api_key].blank? }
+  not_if { node[:elasticsearch][:newrelic][:api_key].to_s.empty? }
 end
