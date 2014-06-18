e0ff3fd94d6e5b1ce645091795c1cd72e8987ba1
Doc Ritezel and Max Petersen <max@wanelo.com>
2014-06-17 17:36:22 -0700

    Bump metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'play@wanelo.com'
 license          'MIT'
 description      'Installs/Configures elasticsearch on Joyent SmartOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.2'
+version          '0.7.3'
 
 depends 'smf', '>= 2.0.4'
