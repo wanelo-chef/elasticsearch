fb7e67bb71e459cc8c0c8d2b5200eaf36450b876
Eric Saxby <eric@wanelo.com>
2014-08-07 17:51:50 -0700

    Bump version to 0.8.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'play@wanelo.com'
 license          'MIT'
 description      'Installs/Configures elasticsearch on Joyent SmartOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.4'
+version          '0.8.0'
 
 depends 'java'
 depends 'smf', '>= 2.0.4'
