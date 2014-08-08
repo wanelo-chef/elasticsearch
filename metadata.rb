8af5f9ea39cf396541e20eca561659ede705a4aa
Eric Saxby <sax@livinginthepast.org>
2014-08-07 21:04:58 -0700

    Bump to 0.8.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'play@wanelo.com'
 license          'MIT'
 description      'Installs/Configures elasticsearch on Joyent SmartOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.0'
+version          '0.8.1'
 
 depends 'java'
 depends 'paths'
