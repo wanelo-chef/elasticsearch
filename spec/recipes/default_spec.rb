9fbe67aae70466c23504eca8102e499e7de47590
Eric Saxby <sax@livinginthepast.org>
2014-12-30 14:27:48 -0800

    Update rspec any_instance syntax

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe 'elasticsearch::default' do
   before do
-    Chef::Recipe.any_instance.stub(:include_recipe)
+    allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
   end
 
   it 'includes the configure recipe' do
-    Chef::Recipe.any_instance.should_receive(:include_recipe).with('elasticsearch::configure')
+    expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('elasticsearch::configure')
     ChefSpec::Runner.new.converge(described_recipe)
   end
 
   it 'includes the install recipe' do
-    Chef::Recipe.any_instance.should_receive(:include_recipe).with('elasticsearch::install')
+    expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('elasticsearch::install')
     ChefSpec::Runner.new.converge(described_recipe)
   end
 end
